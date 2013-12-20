b64238702fc4feb0a13f2feb090da8797aa5a40a
lipanski <florinelul@gmail.com>
2013-12-20 18:03:56 +0100

    Tests for POST /robots

--- lib/dr_roboto/controllers/robots_controller.rb
+++ lib/dr_roboto/controllers/robots_controller.rb
@@ -1,5 +1,9 @@
-module DrRoboto
-  class RobotsController
+require 'dr_roboto/controllers/base_controller'
+require 'dr_roboto/models/inspector'
+require 'dr_roboto/models/robot'
 
+module DrRoboto
+  class RobotsController < BaseController
+    
   end
 end
--- lib/dr_roboto/helpers/params_helper.rb
+++ lib/dr_roboto/helpers/params_helper.rb
@@ -1,5 +1,3 @@
-# require 'sinatra/base'
-
 module DrRoboto
   module ParamsHelper
     
