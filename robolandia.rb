2ccb646b350671099154979322a998df8383b87d
Nuno Pato <nuno.pato@lmit.pt>
2013-12-30 21:25:43 +0000

    Adds Basic HTTP Auth and routes definition for robolandia sinatra app

---
+++ robolandia.rb
@@ -0,0 +1,42 @@
+require 'sinatra/base'
+require 'yaml'
+
+class Robolandia < Sinatra::Base      
+  
+  before do
+    content_type 'application/json'
+  end
+
+  config = YAML.load_file "./auth.yml"
+  database = YAML.load_file "db/database.yml"    
+
+  use Rack::Auth::Basic do |username, password|
+    [username, password] == [config['user'], config['pass']]
+  end
+  
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
+  ActiveRecord::Migration.verbose = true
+  ActiveRecord::Base.establish_connection database[settings.environment.to_s]
+
+  Dir.mkdir("log") unless File.exist?("log")
+  $log = Logger.new("log/robolandia.log", 'daily')
+  $log.level = Logger::DEBUG
+  $log.info "#{Time.now.to_s(:db)} Robolandia is now accepting json requests."
+
+  get '/robots.json' do                
+     
+  end
+
+  get '/robots/:id.json' do    
+    
+  end
+
+  put '/robots/:id.json' do            
+    
+  end
+
+  get '/robots/:id/history.json' do        
+    
+  end
+
+end
