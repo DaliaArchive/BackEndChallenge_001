ff7c7b01c354ef29c282874a42dc8ee968670c75
acaporrini <a.caporrini@gmail.com>
2016-01-26 09:41:19 +0100

    add API route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  namespace :api, defaults: {format: :json} do
+    resources :robots do
+      get 'history', on: :member
+    end
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
