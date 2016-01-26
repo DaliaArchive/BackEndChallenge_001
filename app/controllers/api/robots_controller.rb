66b49dc73ffee9063311d49ce8e26c43f4f44624
acaporrini <a.caporrini@gmail.com>
2016-01-26 11:13:24 +0100

    add history action to robots controller

--- app/controllers/api/robots_controller.rb
+++ app/controllers/api/robots_controller.rb
@@ -3,7 +3,7 @@ class Api::RobotsController < ApplicationController
   protect_from_forgery
   skip_before_action :verify_authenticity_token, if: :json_request?
 
-
+  # Return a list of all robots
   def index
 
     @robots = Robot.all
@@ -17,7 +17,7 @@ class Api::RobotsController < ApplicationController
   end
 
 
-
+  # Return a formatted list of  robot current features
   def show
 
     @robot = Robot.includes(:features).find_by_name(params[:id])
@@ -30,36 +30,53 @@ class Api::RobotsController < ApplicationController
 
   end
 
+  # Return a dettailed list of all revisions and changes for a robot
+  def history
 
+    @robot = Robot.find_by_name(params[:id])
 
+    respond_to do |format|
+
+      format.json { render :json => @robot.history }
+
+    end
+
+  end
+
+  # Update the features of a robot or create a new one if it doesn't exist
   def update
 
     @robot = Robot.find_by_name(params[:id])
 
+    # if robot exist
     if @robot
-
+      # create a revision of type update
       @revision = Revision.create(type:"update")
 
+    # if robot don't exist
     else
-
+      # create a new robot and set up the first revison of type update
       @robot = Robot.create(name: params[:id])
       @revision = Revision.create(type:"create")
 
     end
 
+    Fetch the query string parameters
     params.except(:action, :controller, :format, :id).each do |key, value|
 
+      #add the parameters as new feature associated to the current revision
       @revision.features << Feature.create(name: key, value: value)
 
     end
 
+    #associate the current revision with the robot
     @robot.revisions << @revision
-
+    # save the robot and send the response
     @robot.save
 
     respond_to do |format|
 
-      format.json { render :json => @robot }
+      format.json { render :json => @robot.current_revision }
 
     end
 
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -3,21 +3,16 @@ class Robot < ActiveRecord::Base
   has_many :revisions
   has_many :features, through: :revisions
 
+  # return a formatted list of all the robots with the last update
   def self.list
 
+    # Fetch the robots
     robots = Robot.includes(:revisions).all
 
+    # Format the output
     robots.map do |robot|
 
-      if robot.revisions.empty?
-
-        last_update = robot.created_at.to_formatted_s(:db)
-
-      else
-
-        last_update = robot.revisions.last.created_at.to_formatted_s(:db)
-
-      end
+      last_update = robot.revisions.last.created_at.to_formatted_s(:db)
 
       { name: robot.name, last_update: last_update }
 
@@ -26,18 +21,95 @@ class Robot < ActiveRecord::Base
   end
 
 
-
+  # Return a formatted list of the robot current features
   def current_revision
 
+    # Fetch the robots current features with unique names
     features = self.features.group(:name).select(:name,:value).as_json
 
-    features.map! do |feature|
+    # Format the output
+    features.map do |feature|
       {
         feature["name"] => feature["value"]
       }
     end
 
-    features.to_json
+  end
+
+  # Return a dettailed list of all revisions and changes for a robot
+  def history
+
+    # Fetch the revisions
+    revisions = self.revisions.includes(:features).all
+
+    # Instantiate an empty container for the list
+    history = []
+
+    # Iterate over the features with index in order to grab the previous
+    # element and confront the changes
+    revisions.each_with_index do |actual, i|
+
+      # current features
+      actual_features = actual.get_features
+
+      # Set up the item hash for the current revision
+      created_at = actual.created_at.to_formatted_s(:db)
+
+      revision_item = {
+        created_at => {
+          type: actual.type,
+          changes: []
+
+        }
+      }
+
+      #if it's the first revision
+      if actual.type === "create"
+
+        # set up the changes string with an empty starting value
+        changes = actual_features.map do |name, value|
+
+          "#{name}: [] -> [#{value}]"
+
+        end
+
+        revision_item[created_at][:changes] = changes
+
+      else
+
+        # Fetch the previous revision
+        previous = revisions[i - 1]
+
+        previous_features = previous.get_features
+
+        # Iterate over the features of the current revision to confront them
+        # with the previous revision
+        actual_features.each do |name, value|
+
+          # Feature was already present but the value has changed
+          if previous_features.has_key?(name) && previous_features[name] != value
+            # set up the changes string with the old and new values
+            changes = "#{name}: [#{previous_features[name]}] -> [#{value}]"
+
+          # Feature was not present in the previous revision
+          elsif !previous_features. has_key?(name)
+            # set up the changes string with an empty starting value
+            changes = "#{name}: [] -> [#{value}]"
+
+          end
+
+          # insert the changes in the revision item
+          revision_item[created_at][:changes] << changes
+
+        end
+
+      end
+      #insert the item in the list
+      history << revision_item
+
+    end
+
+    history
 
   end
 
