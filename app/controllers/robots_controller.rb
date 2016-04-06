5335b50d1af68a0faac6c1d6c4c7550c58066338
Tatsuya Suzuki <sin.wave808@gmail.com>
2016-04-06 14:07:52 +0200

    Minor fix for RobotsController

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,20 +1,17 @@
 class RobotsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
-  # GET /robots
   # GET /robots.json
   def index
     @robots = Robot.all
   end
 
-  # GET /robots/1
   # GET /robots/1.json
   def show
     @robot = Robot.find(params[:id])
   end
 
-  # POST /robots
-  # POST /robots.json
+  # POST /robots/maintain.json
   def maintain
     @robot = if robot_params[:id]
         Robot.find(robot_params[:id])
@@ -30,18 +27,13 @@ class RobotsController < ApplicationController
     end
   end
 
+  # GET /robots/1/maintenance_records.json
   def maintenance_records
     @maintenance_records = Robot.find(params[:id]).maintenance_records
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_robot
-    @robot = Robot.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
   def robot_params
     params.require(:robot).permit(:id, :name, features: [:key, :value])
   end
