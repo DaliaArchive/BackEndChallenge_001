7fa31bac8643660df12ca55f1b0fa14698e75939
Filipe Abreu <filabreu@gmail.com>
2016-12-05 17:03:07 -0200

    Embeds many History records in Robot model. Create History when Robot is created or changed. Add AttributeChange to History for each Robot model changed attribute. Set History type to 'create' or 'update' if Robot is created or updated.

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -7,11 +7,14 @@ class Robot
   field :name, type: String
   field :last_update, type: DateTime
 
+  embeds_many :histories
+
   before_save :set_last_update
+  before_save :record_history
 
   def attributes=(values)
     if values.is_a?(Hash)
-      values.each do |key, value|
+      values.except(:last_update).each do |key, value|
         write_attribute(key, value)
       end
     end
@@ -23,5 +26,15 @@ class Robot
 
   def set_last_update
     self.write_attribute(:last_update, DateTime.now)
+  end
+
+  def record_history
+    if self.changed?
+      history = self.histories.build(created_at: self.last_update)
+      history.type = self.new_record? ? 'create' : 'update'
+      self.changes.except(:last_update, :_id).each do |key, values|
+        history.attribute_changes.build(attribute_name: key, old_value: values[0], new_value: values[1])
+      end
+    end
   end
 end
