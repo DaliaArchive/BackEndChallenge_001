b1f74ccc8afb65a870cecdb988a14636020a4eca
Akarsh <akarsh.mit@gmail.com>
2017-11-21 16:06:30 +0100

    Changing Factorygirl to Factorybot in test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   # fixtures :all
-  include FactoryGirl::Syntax::Methods
+  include FactoryBot::Syntax::Methods
 
   # Add more helper methods to be used by all tests here...
 end
